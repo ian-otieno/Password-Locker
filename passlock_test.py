53a6d460be424ec08f01e69384b4d7a39235efba
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-19 12:45:30 +0300

    add class testclass to passlock_test.py

--- passlock_test.py
+++ passlock_test.py
@@ -2,3 +2,16 @@
 import unittest
 from passlock import User
 from passlock import Credentials
+
+class TestClass(unittest.TestCase):
+    """
+    A Test class that defines test cases for the User class.
+    """
+    def setUp(self):
+        """
+        Method that runs before each individual test methods run.
+        """
+        self.new_user = User('IanOtieno','YxR4t1')
+       
+if __name__ == "__main__":
+    unittest.main()
