074f8ee98c8749e2e022b863adfc930e17100201
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-19 09:03:57 +0300

    add class user to passlock.py

--- passlock.py
+++ passlock.py
@@ -2,3 +2,25 @@ import random
 import string
 import pyperclip
 
+class User(object):
+    """
+    Create User class that generates new instances of a user.
+
+    """
+    user_list = []
+
+    def __init__(self, username, password):
+        """
+        method that defines the properties of a user.
+        """
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        """
+        A method that saves a new user instace into the user list
+        """
+        User.user_list.append(self)
+    
+
+   
