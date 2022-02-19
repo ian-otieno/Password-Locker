0684e0b73e0596bb53f337a71e71e373e9b276e6
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-19 13:01:10 +0300

    add test init and save_user to passlock_test.py

--- passlock_test.py
+++ passlock_test.py
@@ -12,6 +12,22 @@ class TestClass(unittest.TestCase):
         Method that runs before each individual test methods run.
         """
         self.new_user = User('IanOtieno','YxR4t1')
+
+    def test_init(self):
+        """
+        test case to chek if the object has been initialized correctly
+        """
+        self.assertEqual(self.new_user.username,'IanOtieno')
+        self.assertEqual(self.new_user.password,'YxR4t1')
+
+    def test_save_user(self):
+        """
+        test case that tests if a new user instance has been saved into the User list
+
+        """
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+     
        
 if __name__ == "__main__":
     unittest.main()
