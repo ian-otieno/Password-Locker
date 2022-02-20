3f358d410e9713f8a7cc8c7125d7956ecb732dfc
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-20 12:26:25 +0300

    add create,save and display user function to run.py

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python3.8
 from passlock import User
 from passlock import Credentials
+
+
+def create_new_user(username, password):
+    '''
+    Function that creates a new user with a username and password
+    '''
+    new_user = User(username, password)
+    return new_user
+
+
+def save_user(user):
+    '''
+    Function that saves a new user
+    '''
+    user.save_user()
+
+
+def display_user():
+    """
+    Function that  displays the existing user
+    """
+    return User.display_user()
+
+
