34bfaaa0d2783ba6ee06d358cabf51a0b716c575
ian_otieno <ian.otieno@student.moringaschol.com>
2022-02-20 08:49:28 +0300

    add testcredentials set up and test initialization

--- passlock_test.py
+++ passlock_test.py
@@ -27,6 +27,25 @@ class TestClass(unittest.TestCase):
         """
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+
+class TestCredentials(unittest.TestCase):
+    """
+    A test class that defines test cases for credentials class
+
+    """ 
+    def setUp(self):
+        """
+        Method that runs prior to each individual credentials test methods run.
+
+        """
+        self.new_credential = Credentials('Gmail','Ian_Otieno','db4Knp')
+    def test_init(self):
+        """
+        Test case to check if a new Credentials instance has been initialized correctly
+        """
+        self.assertEqual(self.new_credential.account,'Gmail')
+        self.assertEqual(self.new_credential.userName,'Ian_Otieno')
+        self.assertEqual(self.new_credential.password,'db4Knp')
      
        
 if __name__ == "__main__":
